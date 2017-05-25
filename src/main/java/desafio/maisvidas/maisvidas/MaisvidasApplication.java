26ec2f74dd64f8abea0bca0f74be1e8426e2bd9c
Erik Paixão <erikpaixao18@gmail.com>
2017-05-25 17:14:09 -0300

    Commit inicial
    Commit inicial

---
+++ src/main/java/desafio/maisvidas/maisvidas/MaisvidasApplication.java
@@ -0,0 +1,12 @@
+package desafio.maisvidas.maisvidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisvidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisvidasApplication.class, args);
+	}
+}
---
+++ src/test/java/desafio/maisvidas/maisvidas/MaisvidasApplicationTests.java
@@ -0,0 +1,16 @@
+package desafio.maisvidas.maisvidas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MaisvidasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
