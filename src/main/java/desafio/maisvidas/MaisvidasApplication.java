67c20970018280c5d211f47b4aa6b7295d4c099d
Erik Paixão <erikpaixao18@gmail.com>
2017-05-25 17:45:08 -0300

    Inserção dos models

--- src/main/java/desafio/maisvidas/MaisvidasApplication.java
+++ src/main/java/desafio/maisvidas/MaisvidasApplication.java
@@ -1,4 +1,4 @@
-package desafio.maisvidas.maisvidas;
+package desafio.maisvidas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
